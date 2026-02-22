e45f2f43942f246ad079e1be85ee059b24f71174
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-02-23 05:16:13 +0800

    Fixed some code, and added FCFS

--- Project1-Lecture.cpp
+++ Project1-Lecture.cpp
@@ -1,37 +1,117 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
+#include <cstdio>
 using namespace std;
 
 struct Process
-    {
-        int Arrival, Burst, Nice;
-    };
+{
+    int Arrival, Burst, Nice;
+};
 
 struct Test
 {
     int NumberOfProcesses;
+    string testAlgorithm;
     Process* ProcessArray;
 };
 
 
 int main(void){
+    /*
+        Initializing test cases, algorithms, and processes
+    */
     int NumberOfTestCases, NumberOfProcesses, Arrival, Burst, Nice;
     string Algorithm;
     Test* TestArray;
     cin >> NumberOfTestCases;
     TestArray = new Test[NumberOfTestCases];
-    for (int i = 0; i == NumberOfTestCases; i++ )
+
+    for (int i = 0; i < NumberOfTestCases; i++)
     {
         cin >> NumberOfProcesses >> Algorithm;
         TestArray[i].ProcessArray = new Process[NumberOfProcesses];
-        for (int j = 0; j == NumberOfProcesses; j++ )
+        TestArray[i].testAlgorithm = Algorithm;
+        TestArray[i].NumberOfProcesses = NumberOfProcesses;
+        for (int j = 0; j < NumberOfProcesses; j++)
         {
-            scanf("%d %d %d", &Arrival, &Burst, &Nice );
+            scanf("%d %d %d", &Arrival, &Burst, &Nice);
             TestArray[i].ProcessArray[j].Arrival = Arrival;
             TestArray[i].ProcessArray[j].Burst = Burst;
             TestArray[i].ProcessArray[j].Nice = Nice;
         }
     }
 
+    /*
+        Executing test cases and algorithms
+    */
+    for (int i = 0; i < NumberOfTestCases; i++)
+    {
+        int totalTime, burstTime = 0;
+        Test currentTest = TestArray[i];
+        string currentAlgo = currentTest.testAlgorithm;
+        Process* readyQueue = currentTest.ProcessArray;
+        int processIndex[currentTest.NumberOfProcesses];
+
+        for (int j = 0; j < currentTest.NumberOfProcesses; j++){
+            processIndex[j] = j;
+        }
+
+        /*
+            Arranging processes according to arrival time for
+            smoother internal execution
+        */
+        for (int j = 0; j < currentTest.NumberOfProcesses - 1; j++)
+        {
+            int minIndex = j;
+            Process minProcess = readyQueue[j];
+
+            for (int k = j + 1; k < currentTest.NumberOfProcesses; k++)
+            {
+                if (readyQueue[k].Arrival < minProcess.Arrival)
+                {
+                    minIndex = k;
+                    minProcess = readyQueue[k];
+                }
+            }
+            Process temp = readyQueue[j];
+            readyQueue[j] = readyQueue[minIndex];
+            readyQueue[minIndex] = temp;
+
+            int tempIndex = processIndex[j];
+            processIndex[j] = processIndex[minIndex];
+            processIndex[minIndex] = tempIndex;
+        }
+
+        if (currentAlgo == "FCFS")
+        {
+            for (int j = 0; j < currentTest.NumberOfProcesses; j++)
+            {
+                Process runningProcess = readyQueue[j];
+
+                totalTime += (runningProcess.Arrival + runningProcess.Burst) - totalTime;
+                burstTime += runningProcess.Burst;
+            }
+
+            cout << to_string(currentTest.NumberOfProcesses) << " " << currentAlgo << endl;
+
+            for (int j = 0; j < currentTest.NumberOfProcesses; j++)
+            {
+                cout << to_string(readyQueue[j].Arrival) << " " << to_string(processIndex[j]);
+                cout << " " << to_string(readyQueue[j].Burst) << "X" << endl;
+            }
+        }
+
+        if (currentAlgo == "SJF")
+        {
+
+        }
+    }
+
+    /*
+        Deallocating memory
+    
+    free(TestArray);
+    TestArray = NULL;
+    return 0;
+    */
 }
