9cec29fbfcf189adea7cb8ac6e5eb972b8c907b7
Jerome Domingo <jerome.domingo@student.ateneo.edu>
2026-03-03 15:14:48 +0800

    Added colors and texture cycling

--- demo1.fs
+++ demo1.fs
@@ -9,14 +9,16 @@
 
 in vec3 shaderColor;
 in vec2 shaderTexCoord;
+uniform float glow1;
+uniform float glow2;
 uniform sampler2D shaderTextureA;
 uniform sampler2D shaderTextureB;
 out vec4 fragmentColor;
 
 void main()
 {
-    vec4 colorA = texture(shaderTextureA, shaderTexCoord);
-    vec4 colorB = texture(shaderTextureB, shaderTexCoord);
+    vec4 colorA = texture(shaderTextureA, shaderTexCoord) * glow1;
+    vec4 colorB = texture(shaderTextureB, shaderTexCoord) * glow2;
     fragmentColor = mix(colorA, colorB, 0.5f);
-    //fragmentColor = vec4(shaderColor, 1.0f) * texture(shaderTextureA, shaderTexCoord);
+    //fragmentColor = vec4(shaderColor, 1.0f) * mix(colorA, colorB, 0.5f);
 }
