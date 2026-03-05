af1e2605da44091d7ea00a04db5b46cc228059de
Jerome Domingo <jmdomingo101405@gmail.com>
2026-03-05 22:11:09 +0800

    Added colors

--- demo1.fs
+++ demo1.fs
@@ -19,6 +19,5 @@ void main()
 {
     vec4 colorA = texture(shaderTextureA, shaderTexCoord) * glow1;
     vec4 colorB = texture(shaderTextureB, shaderTexCoord) * glow2;
-    fragmentColor = mix(colorA, colorB, 0.5f);
-    //fragmentColor = vec4(shaderColor, 1.0f) * mix(colorA, colorB, 0.5f);
+    fragmentColor = vec4(shaderColor, 1.0f) * mix(colorA, colorB, 0.5f);
 }
