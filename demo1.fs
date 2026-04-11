64230364691dac9bc8cdfdd52fc45df57acbece9
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-04-12 05:28:45 +0800

    Added front texture

--- demo1.fs
+++ demo1.fs
@@ -8,9 +8,11 @@
 #version 330 core
 
 in vec3 shaderColor;
+in vec2 shaderTexCoord;
+uniform sampler2D shaderTexture;
 out vec4 fragmentColor;
 
 void main()
 {
-    fragmentColor = vec4(shaderColor, 1.0f);
+    fragmentColor = vec4(shaderColor, 1.0f) * texture(shaderTexture, shaderTexCoord);
 }
