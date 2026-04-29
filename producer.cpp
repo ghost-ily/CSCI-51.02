06b74fa33217cdc0f439040047e0a3bc32fe924b
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-04-30 04:52:18 +0800

    Created thread to listen to ENTER

--- producer.cpp
+++ producer.cpp
@@ -5,16 +5,28 @@
 #include <fstream>
 #include <iostream>
 #include <stdio.h>
+#include <pthread.h>
+#include <thread>
 
 using namespace std;
 
 const int MAX_FRAME_SIZE = 20000; // Maximum size of a video frame
+bool keep_running = true;
 
 struct SharedVideoBuffer {
     int currentFrameNum;
     int totalFrames;
     char frame[MAX_FRAME_SIZE]; // Buffer to hold the video frame
 };
+
+// If input is 10 (ENTER), set keep_running to false
+void *listener(void *ptr) {
+    int input = cin.get();
+    if (input == 10) {
+        keep_running = false;
+    }
+    return NULL;
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -24,6 +36,15 @@ int main(int argc, char* argv[]) {
     
     string file = argv[1];
     int fps = stoi(argv[2]);
+    float sleepTime = 1000 / fps;
+    ifstream inputFile(file);
+
+    // Create thread to take "ENTER" input
+    pthread_t thread;
+    pthread_create(&thread, NULL, listener, NULL);
+
+    // Struct initialization
+    SharedVideoBuffer* vidBuffer = new SharedVideoBuffer;
 
     // Semaphore initialization
     int semID;
@@ -40,59 +61,68 @@ int main(int argc, char* argv[]) {
     // Semaphore accessing
     struct sembuf sem[4];
 
-    // Semaphore 0: Check if producer can write to shared memory
-    sem[0].sem_num = 0;
-    sem[0].sem_op = 0;
-    sem[0].sem_flg = SEM_UNDO;
-
-    // Semaphore 1: Check if producer can run exclusively
-    sem[1].sem_num = 1;
-    sem[1].sem_op = 0;
-    sem[1].sem_flg = SEM_UNDO;
-
-    // Semaphore 0: Producer signals that shared memory will be full 
-    sem[2].sem_num = 0;
-    sem[2].sem_op = 1;
-    sem[2].sem_flg = SEM_UNDO;
-
-    // Semaphore 1: Producer will run exclusively
-    sem[3].sem_num = 1;
-    sem[3].sem_op = 1;
-    sem[3].sem_flg = SEM_UNDO;
-
-    int opResult = semop(semID, sem, 4);
-
-    if (opResult != 1) {
-        // Read ASCII video file and write
-        // frame to shared memory
-    }
-
     // Shared memory initialization
-    int shmID;
     key_t shmKey = 2222;
     int shmSize = sizeof(SharedVideoBuffer); // Size of shared memory segment
     int shmFlag = IPC_CREAT | 0666;
     int shmID = shmget(shmKey, shmSize, shmFlag);
-    sharedMem = (char*)shmat( shmId, NULL, 0 );
 
-    if( ((int*)sharedMem) == (int*)-1 )
-    {
-        perror( "shmop: shmat failed" );
-    }
-    else
-    {
-        const char* buffer = "Hello!";
-        
-        strcpy( sharedMem, buffer );
-        char buffer2[50];
-        
-        strcpy( buffer2, sharedMem );
-        printf( "%s\n", buffer2 );
-        char buffer3[50];
-        
-        memcpy( buffer3, sharedMem, 3 );
-        buffer3[3] = '\0';
-        printf( "%s\n", buffer3 );
+    while(keep_running) {
+        // Semaphore 0: Check if producer can write to shared memory
+        sem[0].sem_num = 0;
+        sem[0].sem_op = 0;
+        sem[0].sem_flg = SEM_UNDO;
+
+        // Semaphore 1: Check if producer can run exclusively
+        sem[1].sem_num = 1;
+        sem[1].sem_op = 0;
+        sem[1].sem_flg = SEM_UNDO;
+
+        // Semaphore 0: Producer signals that shared memory will be full 
+        sem[2].sem_num = 0;
+        sem[2].sem_op = 1;
+        sem[2].sem_flg = SEM_UNDO;
+
+        // Semaphore 1: Producer will run exclusively
+        sem[3].sem_num = 1;
+        sem[3].sem_op = 1;
+        sem[3].sem_flg = SEM_UNDO;
+
+        int opResult = semop(semID, sem, 4);
+        char* sharedMem = (char*)shmat( shmID, NULL, 0 );
+
+        if (opResult != 1) {
+            // Read ASCII video file and write
+            // frame to shared memory
+            if(((int*)sharedMem) == (int*)-1) {
+                perror( "shmop: shmat failed" );
+                exit(1)
+            }
+            else {
+                const char* line_buffer;
+                string currentLine;
+
+                if (inputFile.is_open()) {
+                    // Continuously read each line from
+                    // input file, then ignore the first
+                    // instance of the ASCII ESC character
+                    while (getline(inputFile, currentLine, char(27))) {
+
+                    }
+                }
+                
+                strcpy( sharedMem, line_buffer );
+                char buffer2[50];
+                
+                strcpy( buffer2, sharedMem );
+                printf( "%s\n", buffer2 );
+                char buffer3[50];
+                
+                memcpy( buffer3, sharedMem, 3 );
+                buffer3[3] = '\0';
+                printf( "%s\n", buffer3 );
+            }
+        }
     }
 
     return 0;
